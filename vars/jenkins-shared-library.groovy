1445164bf5297bc429f5dd8ba72a62314fe3969e
KhaledAMohamed <khaledalaa58@yahoo.com>
2023-06-23 04:28:37 +0300

    Update jenkins-shared-library.groovy

--- vars/jenkins-shared-library.groovy
+++ vars/jenkins-shared-library.groovy
@@ -1,5 +1,11 @@
-def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
+pipeline {
+    agent any
+
+    stages {
+        stage('Example') {
+            steps {
+                   sh "echo teeeeeeeest "
+            }
+        }
+    }
 }
